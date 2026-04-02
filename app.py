1d30b5b2fbd27aec263090d1f4ae3236aa04772e
moonsun28 <aahhh7@gmail.com>
2026-04-02 14:47:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import snowflake.connector
 from cryptography.hazmat.primitives import serialization
+import base64
 
 st.title("Cosense App 🚀")
 
 try:
+    private_key_str = st.secrets["snowflake"]["private_key"]
     private_key = serialization.load_pem_private_key(
-        st.secrets["snowflake"]["private_key"].encode(),
+        private_key_str.encode("utf-8"),
         password=None
     )
+
     conn = snowflake.connector.connect(
         user=st.secrets["snowflake"]["user"],
         account=st.secrets["snowflake"]["account"],
@@ -18,9 +21,4 @@ try:
         private_key=private_key.private_bytes(
             serialization.Encoding.DER,
             serialization.PrivateFormat.PKCS8,
-            serialization.NoEncryption()
-        )
-    )
-    st.success("Snowflake 연결 성공")
-except Exception as e:
-    st.exception(e)
+            seriali
