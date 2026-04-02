28b02cb31b771da5605c59a4f8b3a1521114d785
moonsun28 <aahhh7@gmail.com>
2026-04-02 12:51:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+
 import streamlit as st
+import snowflake.connector
 
-conn = st.connection("snowflake")
+st.title("Cosense App 🚀")
 
-def run_query(query):
-    return conn.query(query)
+try:
+    conn = snowflake.connector.connect(
+        user=st.secrets["snowflake"]["user"],
+        password=st.secrets["snowflake"]["password"],
+        account=st.secrets["snowflake"]["account"],
+        host=st.secrets["snowflake"]["host"],
+        warehouse=st.secrets["snowflake"]["warehouse"],
+        database=st.secrets["snowflake"]["database"],
+        schema=st.secrets["snowflake"]["schema"],
+    )
+    st.success("Snowflake 연결 성공")
+except Exception as e:
+    st.exception(e)
+    
